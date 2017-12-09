277b361c567ae72b7aea720967058c62f5d0141d
acamilo <alex.camilo@gmail.com>
2017-12-08 20:24:37 -0500

    Template to insert device ip for camera feed

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def serve_static(filename):
 # make sure index is served for '/'  
 @app.route('/')
 def serve_index():
-  return app.send_static_file('index.html')
+  return return render_template('index.html', myip = "192.168.43.21")
 
 
 if __name__ == '__main__':
